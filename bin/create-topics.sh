9c1cf254a88b2d91f7e81708d53091e43fc38aaa
bbejeck <bbejeck@gmail.com>
2017-12-14 23:39:20 -0500

    updated create topics script

--- bin/create-topics.sh
+++ bin/create-topics.sh
@@ -5,7 +5,7 @@ KAFKA_HOME=$1
 ZK_HOST=$2
 ZK_PORT=$3
 
-topics="src-topic patterns rewards purchases stocks stocks-out transaction-summary coffee electronics transactions customer_transactions events cogrouped-results stock-performance stock-transactions stock-ticker-table stock-ticker-stream stock-volume-by-company companies clients financial-news pops-hops-purchases international-sales domestic-sales stock-counts transaction-count session-transactions"
+topics="src-topic patterns rewards purchases stocks stocks-out transaction-summary coffee electronics transactions customer_transactions events cogrouped-results stock-performance stock-transactions stock-ticker-table stock-ticker-stream stock-volume-by-company companies clients financial-news pops-hops-purchases international-sales domestic-sales stock-counts transaction-count session-transactions sector-transaction-counts session-transactions transaction-count"
 
 for topic in ${topics}; do
      echo "attempting to create topic ${topic}"
