d5f10e273bb9eb0a7ba73448e0780a627e05e1c0
bbejeck <bbejeck@gmail.com>
2018-07-20 21:16:48 -0400

    MINOR: fix to not change ref in method

--- src/main/java/bbejeck/chapter_2/partitioner/PurchaseKeyPartitioner.java
+++ src/main/java/bbejeck/chapter_2/partitioner/PurchaseKeyPartitioner.java
@@ -10,12 +10,12 @@ public class PurchaseKeyPartitioner extends DefaultPartitioner {
 
     @Override
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
-
+        Object newKey = null;
         if (key != null) {
             PurchaseKey purchaseKey = (PurchaseKey) key;
-            key = purchaseKey.getCustomerId();
-            keyBytes = ((String) key).getBytes();
+            newKey = purchaseKey.getCustomerId();
+            keyBytes = ((String) newKey).getBytes();
         }
-        return super.partition(topic, key, keyBytes, value, valueBytes, cluster);
+        return super.partition(topic, newKey, keyBytes, value, valueBytes, cluster);
     }
 }
